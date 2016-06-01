a393b77526f94e14c64c8cad80e037bcf985929d
Fernando Ramires <fernandoplramires@users.noreply.github.com>
2016-06-01 19:51:35 -0300

    Add exceptions class

---
+++ src/main/java/br/com/ramires/palindrome/exception/PalindromeParserException.java
@@ -0,0 +1,9 @@
+package main.java.br.com.ramires.palindrome.exception;
+
+public class PalindromeParserException extends Exception {
+
+	private static final long serialVersionUID = 6266644645481501568L;
+	public PalindromeParserException(final String message) {
+        super(message);
+    }
+}
